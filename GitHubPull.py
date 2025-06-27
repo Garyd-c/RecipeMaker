bd9d942d552285b75e3a0003e95d62e00c2cdfc5
Garrett Christensen <your_email@example.com>
2025-06-27 13:13:54 -0600

    Adding a pull file

---
+++ GitHubPull.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def run_command(command):
+    result = subprocess.run(command, shell=True)
+    if result.returncode != 0:
+        print(f"Command failed: {command}")
+        exit(1)
+
+
+# Step 1: pull updates
+run_command("git pull")
