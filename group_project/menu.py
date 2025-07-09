cec8f870535f77533132ab2fe07ce0496aec7260
Garrett Christensen <your_email@example.com>
2025-07-09 16:39:51 -0600

    Updated menu to handle errors

--- group_project/menu.py
+++ group_project/menu.py
@@ -1,44 +1,53 @@
 import functions as func
 
 while True:
-    selection = int(input("View your recipes here!\n"
-                          "Enter a number from the list below:\n\n"
-                          "1  View recipe List\n"
-                          "2  View a full recipe\n"
-                          "3  View the ingredient list for a recipe\n"
-                          "4  Create new recipe\n"
-                          "5  Update a recipe\n"
-                          "6  Delete a recipe\n"
-                          "7  Close program\n\n"
-                          "What would you like to do? "))
+    try:    
+        selection = int(input("View your recipes here!\n"
+                            "Enter a number from the list below:\n\n"
+                            "1  View recipe List\n"
+                            "2  View a full recipe\n"
+                            "3  View the ingredient list for a recipe\n"
+                            "4  Create new recipe\n"
+                            "5  Update a recipe\n"
+                            "6  Delete a recipe\n"
+                            "7  Close program\n\n"
+                            "What would you like to do? "))
 
 
-    if selection == 1:
-        # View recipe List
-        pass
+        if selection == 1:
+            # View recipe List
+            pass
 
-    elif selection == 2:
-        # View a full recipe
-        pass
+        elif selection == 2:
+            # View a full recipe
+            pass
 
-    elif selection == 3:
-        # View the ingredient list for a recipe
-        pass
+        elif selection == 3:
+            # View the ingredient list for a recipe
+            pass
 
-    elif selection == 4:
-        # Create new recipe
-        pass
+        elif selection == 4:
+            # Create new recipe
+            pass
 
-    elif selection == 5:
-        # Update a recipe
-        pass
+        elif selection == 5:
+            # Update a recipe
+            pass
 
-    elif selection == 6:
-        # Delete a recipe
-        pass
+        elif selection == 6:
+            # Delete a recipe
+            pass
 
-    elif selection == 7:
-        print("Program Closed. Have a great day!")
-        break
+        elif selection == 7:
+            print("Program Closed. Have a great day!")
+            break
+
+        else:
+            print("The number you entered is not in the list.")
+
+    except Exception as e:
+            print("An error has occurred.", e)
+
 
     input("\nPress 'Enter' to return to menu")
+    
